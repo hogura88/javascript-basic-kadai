5da7c3abe0b36667b2ec043beb0716ba78b40599
hogura88 <hogura88@gmail.com>
2024-03-26 20:26:28 +0900

    14章スコープ演習

--- js/function.js
+++ js/function.js
@@ -13,6 +13,29 @@ sayGoodMorning();
 
 sayGoodEvning();
 
+const calculateTotal = (price) => {
+    console.log(price + 500 + '円');
+}
+
+calculateTotal(1200);
+
 const addTwoArguments = (price, shippingFee) => {
     console.log(price + shippingFee + '円');
 }
+
+addTwoArguments(1200,500);
+
+const double = (num) => {
+    return num * 2;
+}
+
+console.log(double(30));
+
+const useVariable = () => {
+    const userName = '侍太郎';
+    console.log(userName);
+}
+
+useVariable();
+
+console.log(userName);
