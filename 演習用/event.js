227c390a28ca4bff93a32a20a4c0481276f8e3bd
hogura88 <hogura88@gmail.com>
2024-03-29 17:04:40 +0900

    19章演習

--- 演習用/event.js
+++ 演習用/event.js
@@ -16,3 +16,28 @@ addBtn.addEventListener('click', () => {
     parentList.appendChild(childList);
 });
 
+const countBtn = document.getElementById('count-btn');
+
+countBtn.addEventListener('click', () => {
+    const text = document.forms.textForm.textBox.value;
+    console.log(text.length + '文字');
+});
+
+const areaBtn = document.getElementById('area-btn');
+
+areaBtn.addEventListener('click', () => {
+    const area = document.forms.areaForm.area.value;
+    console.log(area);
+});
+
+const osBtn = document.getElementById('os-btn');
+
+osBtn.addEventListener('click', () => {
+    const items = document.forms.osForm.os;
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].checked) {
+            console.log(items[i].value);
+        }
+    }
+});
+
