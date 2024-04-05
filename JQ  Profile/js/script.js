ea00f5ff03670cb0410969075dc0789aae982495
hogura88 <hogura88@gmail.com>
2024-04-06 05:59:27 +0900

    JQポートフォリオ1回目終了

---
+++ JQ  Profile/js/script.js
@@ -0,0 +1,65 @@
+$(function(){
+
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        fade: true,
+        speed: 1500,
+        pauseOnHover: false
+    });
+
+    $('a').hover(
+        function(){$(this).animate({ 'opacity': 0.6}, 300);
+    },
+        function(){$(this).animate({ 'opacity': 1.0}, 300);
+    }
+    );
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#page-top').fadeIn();
+        } else {
+            $('#page-top').fadeOut();
+        }
+    });
+
+    $('a[href^="#"]').click(function(){
+        const speed = 500;
+        const href = $(this).attr('href');
+        let $target;
+        if (href == '#') {
+            $target = $('html');
+        }
+        else {
+            $target = $(href);
+        }
+        const position =$target.offset().top;
+        $('html,body').animate({'scrollTop':position},speed,'swing');
+        return false;
+    });
+    
+    $(window).scroll(function(){
+        const scrollAmount = $(window).scrollTop();
+        const windowHeight = $(window).height();
+        $('section').each(function(){
+            const position = $(this).offset().top;
+            if (scrollAmount > position - windowHeight + 100) {
+                $(this).addClass('fade-in');
+            }
+        });
+    });
+
+    $('.works img').click(function(){
+        const imgSrc = $(this).attr('src');
+        $('.big-img').attr('src', imgSrc);
+        $('.modal').fadeIn();
+        return false
+    });
+
+    $('.close-btn').click(function(){
+        $('.modal').fadeOut();
+        return false
+    });
+
+});
