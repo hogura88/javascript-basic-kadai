f4ad12f9ada13bb68bb9c00410ff331469c6c86e
hogura88 <hogura88@gmail.com>
2024-06-24 01:35:01 +0900

    JSreview

---
+++ js2/data.js
@@ -0,0 +1,13 @@
+console.log(45 + 18);
+console.log(45 % 8);
+console.log('今日の天気は' + '晴れ');
+
+let userName;
+userName = '侍太郎';
+let userNunber = 55;
+console.log(userName);
+console.log(userNunber);
+const shippingFee = 500;
+console.log(shippingFee);
+console.log(45 < 18);
+
